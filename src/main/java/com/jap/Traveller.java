508d93e0211230cce53150a23b5ca0adb880952f
M V K <krishna.mugala9@gmail.com>
2022-08-22 19:59:54 +0530

    added a method  "toSortCityarray" to sort the string array

--- src/main/java/com/jap/Traveller.java
+++ src/main/java/com/jap/Traveller.java
@@ -3,8 +3,17 @@ package com.jap;
 public class Traveller {
     public static void main(String[] args) {
 
-        String[] Names = {"Bern", "Lucerne", "Interlaken", "Grindelwald", "Engelberg", "Geneva", "Murren", "Basel", "Zermatt", "Jungfraujoch"};
-        int[] KilometerArray = {138, 52, 118, 136, 85, 276, 103, 87, 214, 101};
+        // Creating an array of strings.
+        String[] City = {"Bern", "Lucerne", "Interlaken", "Grindelwald", "Engelberg", "Geneva", "Murren", "Basel", "Zermatt", "Jungfraujoch"};
+        // Creating an array of integers.
+        int[] distance = {138, 52, 118, 136, 85, 276, 103, 87, 214, 101};
+
 
     }
+
+    public String toSortCityarray() {
+
+        return null;
+    }
+
 }
