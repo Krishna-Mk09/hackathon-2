407ed1e68472b677cce42c1b0731758c01d92c06
M V K <krishna.mugala9@gmail.com>
2022-08-22 19:54:28 +0530

    added String array in the main method

--- src/main/java/com/jap/Traveller.java
+++ src/main/java/com/jap/Traveller.java
@@ -3,5 +3,8 @@ package com.jap;
 public class Traveller {
     public static void main(String[] args) {
 
+        String[] Names = {"Bern", "Lucerne", "Interlaken", "Grindelwald", "Engelberg", "Geneva", "Murren", "Basel", "Zermatt", "Jungfraujoch"};
+
+
     }
 }
