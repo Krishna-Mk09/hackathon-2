bb7bb1187370011ccd843d1ef0c28cef6b4b833b
M V K <krishna.mugala9@gmail.com>
2022-08-22 19:55:07 +0530

    added int array in the main method

--- src/main/java/com/jap/Traveller.java
+++ src/main/java/com/jap/Traveller.java
@@ -4,7 +4,7 @@ public class Traveller {
     public static void main(String[] args) {
 
         String[] Names = {"Bern", "Lucerne", "Interlaken", "Grindelwald", "Engelberg", "Geneva", "Murren", "Basel", "Zermatt", "Jungfraujoch"};
-
+        int[] KilometerArray = {138, 52, 118, 136, 85, 276, 103, 87, 214, 101};
 
     }
 }
