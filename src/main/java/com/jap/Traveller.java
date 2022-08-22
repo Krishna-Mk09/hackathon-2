04057bacfdb2110b8f4fa64679e53b696d073e78
M V K <krishna.mugala9@gmail.com>
2022-08-22 20:09:56 +0530

    added logic to upper case method

--- src/main/java/com/jap/Traveller.java
+++ src/main/java/com/jap/Traveller.java
@@ -4,7 +4,7 @@ public class Traveller {
     public static void main(String[] args) {
 
         // Creating an array of strings.
-        String[] City = {"Bern", "Lucerne", "Interlaken", "Grindelwald", "Engelberg", "Geneva", "Murren", "Basel", "Zermatt", "Jungfraujoch"};
+        String[] Cities = {"Bern", "Lucerne", "Interlaken", "Grindelwald", "Engelberg", "Geneva", "Murren", "Basel", "Zermatt", "Jungfraujoch"};
         // Creating an array of integers.
         int[] distance = {138, 52, 118, 136, 85, 276, 103, 87, 214, 101};
 
@@ -16,13 +16,23 @@ public class Traveller {
         return null;
     }
 
-    public String toUpperCase() {
+    public String toUpperCase(String cities) {
 
-        return null;
+        return cities.toUpperCase();
     }
 
     public int toSortDistanceArray() {
 
         return 0;
+    }
+
+    public String toFindNearestCity() {
+
+        return null;
+    }
+
+    public String toFindFarestCity() {
+
+        return null;
     }
 }
