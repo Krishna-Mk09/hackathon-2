90919dc476306fac0888f0b16fcd4f34d0382c3e
M V K <krishna.mugala9@gmail.com>
2022-08-23 22:29:06 +0530

    added test cases for the program

--- src/test/java/com/jap/TravellerTest.java
+++ src/test/java/com/jap/TravellerTest.java
@@ -5,65 +5,90 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-
-class TravellerTest {
-    Traveller input;
+public class TravellerTest {
+    Traveller traveller;
+    String[] cityNames;
+    int[] distance;
 
     @BeforeEach
-    void setUp() {
-        input = new Traveller();
-
+    public void setUp() {
+        traveller = new Traveller();
+        cityNames = new String[]{"Bern", "Lucerne", "Interlaken", "Grindelwald", "Engelberg", "Geneva", "Murren", "Basel", "Zermatt", "Jungfraujoch"};
+        distance = new int[]{138, 52, 118, 136, 85, 276, 103, 87, 214, 101};
     }
 
     @AfterEach
-    void tearDown() {
-
+    public void tearDown() {
     }
 
-    @Test
-    void testMain() {
-    }
-
-    @Test
-    void testSortCityArray() {
-        String[] names = {"Bern", "Lucerne", "Interlaken", "Grindelwald", "Engelberg", "Geneva", "Murren", "Basel", "Zermatt", "Jungfraujoch"};
+    @Test    //write all the test cases here
+    public void checkSortedCitiesInAlphabeticalOrderSuccess() {
         String[] expected = {"Basel", "Bern", "Engelberg", "Geneva", "Grindelwald", "Interlaken", "Jungfraujoch", "Lucerne", "Murren", "Zermatt"};
-        String[] actualresult = Traveller.toSortCityArray(names);
-        Assertions.assertEquals(expected, actualresult);
+        Assertions.assertEquals(expected[0], Traveller.toSortCityArray(cityNames)[0]);
+        Assertions.assertEquals(expected[1], Traveller.toSortCityArray(cityNames)[1]);
+        Assertions.assertEquals(expected[2], Traveller.toSortCityArray(cityNames)[2]);
+    }
+
+    @Test    //write all the test cases here
+    public void checkSortedCitiesInAlphabeticalOrderFailure() {
+        String[] expected = {"Basel", "Bern", "Engelberg", "Geneva", "Grindelwald", "Interlaken", "Jungfraujoch", "Lucerne", "Murren", "Zermatt"};
+        Assertions.assertNotEquals(expected[0], Traveller.toSortCityArray(cityNames)[3]);
+        Assertions.assertNotEquals(expected[0], Traveller.toSortCityArray(cityNames)[2]);
+        Assertions.assertNotEquals(expected[0], Traveller.toSortCityArray(cityNames)[5]);
     }
 
     @Test
-    void testSortDistanceArray() {
-        int[] distance = {138, 52, 118, 136, 85, 276, 103, 87, 214, 101};
+    public void checkSortedDistanceInAscendingOrderSuccess() {
         int[] expected = {52, 85, 87, 101, 103, 118, 136, 138, 214, 276};
-        int[] actual = input.toSortDistanceArray(distance);
-        Assertions.assertEquals(expected, actual);
+        Assertions.assertEquals(expected[0], traveller.toSortDistanceArray(distance)[0]);
+        Assertions.assertEquals(expected[1], traveller.toSortDistanceArray(distance)[1]);
+        Assertions.assertEquals(expected[2], traveller.toSortDistanceArray(distance)[2]);
     }
 
     @Test
-    void testToFindNearestCity() {
-        int number = 52;
-
-        String expected = "Lucerne";
-        String actualresult = input.toFindNearestCity(number);
-        Assertions.assertEquals(expected, actualresult);
-
+    public void checkSortedDistanceInAscendingOrderFailure() {
+        int[] expected = {52, 85, 87, 101, 103, 118, 136, 138, 214, 276};
+        Assertions.assertNotEquals(expected[0], traveller.toSortDistanceArray(distance)[6]);
+        Assertions.assertNotEquals(expected[0], traveller.toSortDistanceArray(distance)[1]);
+        Assertions.assertNotEquals(expected[0], traveller.toSortDistanceArray(distance)[2]);
     }
 
     @Test
-    void testToFindFartestCity() {
-        int number = 276;
-        String expected = "Geneva";
-        String actual = input.toFindNearestCity(number);
-        Assertions.assertEquals(expected, actual);
-
-    }
-
-    @Test
-    void testToUpperCase() {
+    public void cityNamesConvertedToUpperCaseSuccess() {
         String name = "bern";
         String expected = "BERN";
-        String actual = input.toUpperCase(name);
+        String actual = traveller.toUpperCase(name);
         Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void cityNamesConvertedToUpperCaseFailure() {
+        String name = "bern";
+        String expected = "berN";
+        String actual = traveller.toUpperCase(name);
+        Assertions.assertNotEquals(expected, actual);
+    }
+
+    @Test
+    public void cityWhichIs52kmsFromZurichSuccess() {
+        Assertions.assertEquals("Lucerne", traveller.toFindNearestCity(distance, cityNames));
+    }
+
+    @Test
+    public void cityWhichIs52kmsFromZurichFailure() {
+        Assertions.assertNotEquals("Basel", traveller.toFindNearestCity(distance, cityNames));
+        Assertions.assertNotEquals("Geneva", traveller.toFindNearestCity(distance, cityNames));
+    }
+
+    @Test
+    public void cityWhichIsGreaterThan270kmsFromZurichSuccess() {
+        Assertions.assertEquals("Geneva", traveller.toFindFartestCity(distance, cityNames));
+    }
+
+    @Test
+    public void cityWhichIsGreaterThan270kmsFromZurichFailure() {
+        Assertions.assertNotEquals("Basel", traveller.toFindFartestCity(distance, cityNames));
+        Assertions.assertNotEquals("Lucerne", traveller.toFindFartestCity(distance, cityNames));
+
     }
 }
